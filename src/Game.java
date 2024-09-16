35167ebc14e6c17ce7dd664b68a5bb5deedce49a
rodro <RODROX203@gmail.com>
2024-09-16 15:46:24 -0600

    1er avance 16/09/24 15:47

---
+++ src/Game.java
@@ -0,0 +1,13 @@
+public class Game {
+    public static void game(String[] args) {
+        Player player1 = new Player("Jugador/a");
+        player1.addStat(Stats.HP, 100);
+        player1.addStat(Stats.ATTACK, 20);
+
+        Enemy enemy1 = new Enemy("El Bryan");
+        enemy1.addStat(Stats.HP, 200);
+        enemy1.addStat(Stats.DEFENSE, 15);
+
+        // Aquí puedes agregar más estadísticas y lógica de juego
+    }
+}
---
+++ src/Player.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+
+public class Player {
+    private String name;
+    private HashMap<Stats, Integer> stats;
+
+    public Player(String name) {
+        this.name = name;
+        this.stats = new HashMap<>();
+    }
+
+    // Método para agregar una estadística al jugador
+    public void addStat(Stats stat, int value) {
+        stats.put(stat, value);
+    }
+    public String getName() { //Metodo para nombrar
+        return name;
+    }
+    public void attack(Enemy enemy) {
+        // Logica de ataqie
+    }
+
+    // Otros métodos y lógica específica del jugador
+}
