957b0af6b1fc38cb0f882e3dd5704b7421068744
Neri0610 <alex7710ne@gmail.com>
2024-09-20 17:57:11 -0600

    Update Enemy.java

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,12 +1,19 @@
 import java.util.HashMap;
 
-public class Enemy {
+public class Enemy { 
     private String name;
     private HashMap<Stats, Integer> stats;
 
     public Enemy(String name) {
         this.name = name;
         this.stats = new HashMap<>();
+        this.stats.put(Stats.max_HP, 100);
+        this.stats.put(Stats.HP, 100);
+        this.stats.put(Stats.max_HP, 50);
+        this.stats.put(Stats.MP, 50);
+        this.stats.put(Stats.ATTACK, 10);
+        this.stats.put(Stats.DEFENSE, 5);
+        
     }
 
     // Método para agregar una estadística al enemigo
@@ -17,6 +24,15 @@ public class Enemy {
         return name;
         }
     public void attack(Player player) {
-        // Logica de ataqie
+        int damage = this.stats.get(Stats.ATTACK) - player.getStats().get(Stats.Defense);
+        if(damage>0){
+            player.getStats().put(States.HP, player.getStats().get(Stats.HP) - damage);
+            System.out.print1n(this.name + "attacks" +player.getName() + "for" + damage + "damage!");
+        }else{
+            System.out.print1n(this.name + "attacks" +player.getName() + "but does no damage!");
+        }
+    }
+    public boolean isAlive(){
+        return this.stats.get(Stats.HP)>0;
     }
 }
