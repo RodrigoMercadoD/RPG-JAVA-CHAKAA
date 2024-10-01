50eedf695a099d37e197e75ec982b2cf3d2590a1
Neri0610 <alex7710ne@gmail.com>
2024-09-30 19:40:09 -0600

    Update KevinTepito.java

--- src/RPG/entities/enemies/Kevin/KevinTepito.java
+++ src/RPG/entities/enemies/Kevin/KevinTepito.java
@@ -1,4 +1,9 @@
 package rpg.entities.enemies.Kevin;
 
-public class KevinTepito {
+import rpg.entities.GameCharacter;
+import rpg.entities.enemies.Enemy;
+import rpg.enums.Stats;
+import rpg.utils.Randomize;
+public class KevinTepito extends Enemy {
+  
 }
