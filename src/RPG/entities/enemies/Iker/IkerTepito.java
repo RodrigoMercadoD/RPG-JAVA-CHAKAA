4d23c33f041bf662487345075ca6d16f819c8d7f
Luiguileshuga19 <luigiperry2001@gmail.com>
2024-10-01 21:11:28 -0600

    Update IkerTepito.java

--- src/RPG/entities/enemies/Iker/IkerTepito.java
+++ src/RPG/entities/enemies/Iker/IkerTepito.java
@@ -15,26 +15,26 @@ public class IkerTepito extends Enemy {
         this.stats.put(Stats.DEFENSE, 4);
     }
 
-    protected void splash(GameCharacter enemy) {
+    protected void Michelada(GameCharacter enemy) {
 
-        System.out.println(this.name + " splashes " + enemy.getName() + " and does nothing.");
+        System.out.println(this.name + " Avieinta su miche " + enemy.getName() + " pero no le atina.");
         System.out.println(enemy.getName() + " has " + enemy.getStats().get(Stats.HP) + " HP left.");
     }
 
-    protected void trhowSlime(GameCharacter enemy) {
+    protected void Cawamaso(GameCharacter enemy) {
 
         int damage = (int) (this.stats.get(Stats.ATTACK) * 0.8);
         enemy.getStats().put(Stats.HP, enemy.getStats().get(Stats.HP) - damage);
-        System.out.println(this.name + " throws slime at " + enemy.getName() + " for " + damage + " damage!");
+        System.out.println(this.name + " Avienta su botella de cawama " + enemy.getName() + " Victoria " + damage + " y te descuenta!");
         System.out.println(enemy.getName() + " has " + enemy.getStats().get(Stats.HP) + " HP left.");
     }
 
     @Override
     public void attack(GameCharacter enemy) {
         if (Math.random() < 0.5) {
-            splash(enemy);
+            Michelada(enemy);
         } else {
-            trhowSlime(enemy);
+            Cawamaso(enemy);
         }
     }
 }
